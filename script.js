5305ccc0e79d52eeb2297fd6bd9601ad038fc460
Santiago Fontana <santiagofontana61@gmail.com>
2024-10-09 00:08:56 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("HTML");
+        habilidades[2].classList.add("CSS");
+        habilidades[3].classList.add("GIT");
+        habilidades[4].classList.add("Python");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("project");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+} 
